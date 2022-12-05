f9994193eaf663096b4c69b779cc4d977fb171a6
GiuseppeTT <giu.tinti@gmail.com>
2022-12-05 00:47:50 +0000

    Add remote store fot state file

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = "~> 1.3"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -6,5 +8,10 @@ terraform {
     }
   }
 
-  required_version = "~> 1.3"
+  cloud {
+    organization = "giutt-org"
+    workspaces {
+      name = "test-app-budget-terraform-cloud-workspace"
+    }
+  }
 }
