a4c88b3cc06010386a4bada40c946092032af7f8
GiuseppeTT <giu.tinti@gmail.com>
2022-12-06 05:32:15 +0000

    Fix test app CI/DI

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "database_fqdn" {
+  description = "The database FQND"
+  value       = azurerm_postgresql_flexible_server.this.fqdn
+}
+
+
 output "api_docs" {
   description = "The API docs as accessed from the container's FQND"
   value       = "${azurerm_container_group.this.fqdn}/docs"
